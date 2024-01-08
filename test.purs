e8d39cb70c9047bc0bb1648418fab8a73fb9c8f6
Nguyễn Doãn Đại <40011497+MegumiTadokoro@users.noreply.github.com>
2024-01-08 14:03:29 +0100

    Prepapring code production

--- test.purs
+++ test.purs
@@ -1,12 +1,16 @@
 module Main where
-
 import Prelude
 import Effect
 import Effect.Console
 
-fact:: Int -> Int
-fact n = if n <= 1 then 1 else n * fact (n-1)
+
+class A where
+  foo:: Int -> String
+class B where
+  bar:: Int -> String
+
+instance A => B where
+  bar n = foo n
 
 main :: Effect Unit
-main = log (show (fact 10))
-
+main = log "ok"
