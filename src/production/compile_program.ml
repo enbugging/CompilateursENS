175e082c73079d7dee433984bc479610dc86a118
Nguyễn Doãn Đại <40011497+MegumiTadokoro@users.noreply.github.com>
2024-01-19 18:43:13 +0100

    Outline for compile_program.ml

--- src/production/compile_program.ml
+++ src/production/compile_program.ml
@@ -1,37 +1,23 @@
-(******************************************************************************)
-(* phase 2 : production de code *)
+open Past
+open Alloc
+open Compile_expr
 
-let popn n = addq (imm n) !%rsp
-let pushn n = subq (imm n) !%rsp                        
+let env = int Smap.t
 
-let compile_stmt (codefun, codemain) = function
-
+let rec compile_stmt (code, data) statement = 
+  match statement with
+  | PTypeDeclaration (_, _, _, _, def) -> 
+    let PDefinition (label, _, expr) = def in 
+    compile_expr env (code, data) expr
+  | PData _ -> () (* TODO *)
+  | PClass _ -> () 
+  | PInstance _ -> () (* TODO *)
 
 let compile_program p ofile =
-  let p = alloc p in
-  Format.eprintf "%a@." print p;
-  let codefun, code = List.fold_left compile_stmt (nop, nop) p in
-  let p =
-    { text =
-        globl "main" ++ label "main" ++
-        movq !%rsp !%rbp ++
-        code ++
-        movq (imm 0) !%rax ++ (* exit *)
-        ret ++
-        label "print_int" ++
-        movq !%rdi !%rsi ++
-        movq (ilab ".Sprint_int") !%rdi ++
-        movq (imm 0) !%rax ++
-        call "printf" ++
-        ret ++
-        codefun;
-      data =
-        Hashtbl.fold (fun x _ l -> label x ++ dquad [1] ++ l) genv
-          (label ".Sprint_int" ++ string "%d\n")
-    }
-  in
-  let f = open_out ofile in
-  let fmt = formatter_of_out_channel f in
-  X86_64.print_program fmt p;
-  fprintf fmt "@?";
-  close_out f
+    let PFile decl_list = alloc env p in
+    let p = List.fold_left compile_stmt (nop, nop) decl_list in
+    let f = open_out ofile in
+    let fmt = formatter_of_out_channel f in
+    X86_64.print_program fmt p;
+    fprintf fmt "@?";
+    close_out f
